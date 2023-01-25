64702233641f54b0d65b8d0ef48cf11f15e9e05b
Serenajandra <serena.jandra@gmail.com>
2023-01-25 11:15:37 +0100

    completata pagina index

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -1,13 +1,33 @@
 @extends('layouts.admin')
 @section('content')
-    <h4>sono la pagina index</h4>
+    <h1 class="text-center my-4 text-uppercase">Lista dei progetti</h1>
     <div class="container">
-        <ul>
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">id</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Presentazione</th>
+                    <th scope="col">Contenuto</th>
+                    <th scope="col">Azioni</th>
 
-            @foreach ($projects as $project)
-                <li> {{ $project->title }} </li>
-            @endforeach>
-
-        </ul>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($projects as $project)
+                    <tr>
+                        <th scope="row">{{ $project->id }}</th>
+                        <td>{{ $project->title }}</td>
+                        <td>{{ $project->presentation }}</td>
+                        <td>{{ $project->content }}</td>
+                        <td>
+                            <a class="btn btn-warning" href="">Dettagli</a>
+                            <a class="btn btn-dark" href="">Modifica</a>
+                            <a class="btn btn-danger" href="">Cancella</a>
+                        </td>
+                    </tr>
+                @endforeach>
+            </tbody>
+        </table>
     </div>
 @endsection
