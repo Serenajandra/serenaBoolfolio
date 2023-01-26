67ec0d2c1f3376ac29e62d52b5d59e78e6e480ad
Serenajandra <serena.jandra@gmail.com>
2023-01-26 17:07:19 +0100

    aggiornata pagina create ed edit

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -9,19 +9,18 @@
             @csrf
             <div class="my-3 form-group">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" name="title" class="form-control" id="title" value="">
+                <input type="text" name="title" class="form-control" id="title">
             </div>
 
 
             <div class="mb-3 form-group">
                 <label for="presentation" class="form-label">Presentazione</label>
-                <textarea type="text" name="presentation" class="form-control" id="presentation" value="" rows="5"></textarea>
+                <textarea name="presentation" class="form-control" id="presentation" rows="5"></textarea>
             </div>
 
             <div class="mb-3 form-group">
                 <label for="content" class="form-label">Contenuto</label>
-                <textarea row="" type="text" name="content" class="form-control" id="content" value=""
-                    rows="10"></textarea>
+                <textarea name="content" class="form-control" id="content" rows="10"></textarea>
             </div>
             {{-- <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -1,31 +1,31 @@
 @extends('layouts.admin')
 @section('content')
     <h2 class="text-center mt-4">Modifica il progetto</h2>
+
     <form class="container" action="{{ route('admin.projects.update', $project->id) }}" method="POST">
         @csrf
         @method('PUT')
 
         <div class="my-3 form-group">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" value="{{ $project->title }}">
+            <input type="text" class="form-control" id="title" name="title" value="{{ $project->title }}">
         </div>
 
 
         <div class="mb-3 form-group">
             <label for="presentation" class="form-label">Presentazione</label>
-            <textarea type="text" class="form-control" id="presentation" value="{{ $project->presentation }}" rows="5">{{ $project->presentation }}</textarea>
+            <textarea class="form-control" id="presentation" name="presentation" rows="5">{{ $project->presentation }}</textarea>
         </div>
 
         <div class="mb-3 form-group">
             <label for="content" class="form-label">Contenuto</label>
-            <textarea row="" type="text" class="form-control" id="content" value="{{ $project->content }}"
-                rows="10">{{ $project->content }}</textarea>
+            <textarea class="form-control" id="content" rows="10" name="content">{{ $project->content }}</textarea>
         </div>
         {{-- <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
             <label class="form-check-label" for="exampleCheck1">Check me out</label>
         </div> --}}
-        <button type="submit" class="btn btn-primary mt-3 mb-5">Conferma
+        <button type="submit" value="invia" class="btn btn-primary mt-3 mb-5">Conferma
 
         </button>
     </form>
