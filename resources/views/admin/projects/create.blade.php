cdb35ce026812c2d31ddc66a80e6f86e78606840
Serenajandra <serena.jandra@gmail.com>
2023-01-28 11:01:34 +0100

    aggiunta funzione old nelle  form di edit e create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -16,18 +16,18 @@
             @csrf
             <div class="my-3 form-group">
                 <label for="title" class="form-label">Titolo</label>
-                <input type="text" name="title" class="form-control" id="title">
+                <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
             </div>
 
 
             <div class="mb-3 form-group">
                 <label for="presentation" class="form-label">Presentazione</label>
-                <textarea name="presentation" class="form-control" id="presentation" rows="5"></textarea>
+                <textarea name="presentation" class="form-control" id="presentation" rows="5">{{ old('presentation') }}</textarea>
             </div>
 
             <div class="mb-3 form-group">
                 <label for="content" class="form-label">Contenuto</label>
-                <textarea name="content" class="form-control" id="content" rows="10"></textarea>
+                <textarea name="content" class="form-control" id="content" rows="10">{{ old('content') }}</textarea>
             </div>
             {{-- <div class="mb-3 form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
--- resources/views/admin/projects/edit.blade.php
+++ resources/views/admin/projects/edit.blade.php
@@ -20,18 +20,19 @@
 
         <div class="my-3 form-group">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{ $project->title }}">
+            <input type="text" class="form-control" id="title" name="title"
+                value="{{ old('title', $project->title) }}">
         </div>
 
 
         <div class="mb-3 form-group">
             <label for="presentation" class="form-label">Presentazione</label>
-            <textarea class="form-control" id="presentation" name="presentation" rows="5">{{ $project->presentation }}</textarea>
+            <textarea class="form-control" id="presentation" name="presentation" rows="5">{{ old('presentation', $project->presentation) }}</textarea>
         </div>
 
         <div class="mb-3 form-group">
             <label for="content" class="form-label">Contenuto</label>
-            <textarea class="form-control" id="content" rows="10" name="content">{{ $project->content }}</textarea>
+            <textarea class="form-control" id="content" rows="10" name="content">{{ old('content', $project->content) }}</textarea>
         </div>
         {{-- <div class="mb-3 form-check">
             <input type="checkbox" class="form-check-input" id="exampleCheck1">
